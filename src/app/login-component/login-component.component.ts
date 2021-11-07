bc9495cc4312d5a0b6851e15d029ff50d254a7ad
bharatharun <bharatharun28@gmail.com>
2021-11-07 12:04:15 +0530

    login page

---
+++ src/app/login-component/login-component.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login-component',
+  templateUrl: './login-component.component.html',
+  styleUrls: ['./login-component.component.css']
+})
+export class LoginComponentComponent implements OnInit {
+  showLoginForm=true;
+  showSignupForm=false;
+  constructor() { }
+
+  ngOnInit(): void {
+    
+  }
+  login(){
+    this.showLoginForm=true;
+    this.showSignupForm=false;
+  }
+  signup(){
+    this.showLoginForm=false;
+    this.showSignupForm=true;
+  }
+  showTime(){
+    alert("hi ");
+  }
+}
