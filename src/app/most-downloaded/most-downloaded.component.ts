1122770dc47d6d0073c9227b0a0feaa91aeec794
bharatharun <bharatharun28@gmail.com>
2021-11-08 19:06:29 +0530

    image array done

--- src/app/most-downloaded/most-downloaded.component.ts
+++ src/app/most-downloaded/most-downloaded.component.ts
@@ -7,9 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MostDownloadedComponent implements OnInit {
 
+  images:any=[];
   constructor() { }
 
   ngOnInit(): void {
+    this.images=[
+      {image:"assets/blog-img212.jpg"},
+      {image:"assets/blog-img218.jpg"},
+      {image:"assets/blog-img220.jpg"},
+      {image:"assets/blog-img219.jpg"},
+      {image:"assets/blog-img211.jpg"},
+      {image:"assets/blog-img222.jpg"}
+    ]
   }
 
 }
--- src/app/profileComponent/profile-component.component.ts
+++ src/app/profileComponent/profile-component.component.ts
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile-component.component.css']
 })
 export class ProfileComponent implements OnInit {
-
+  images:any =[];
   constructor() { }
 
   ngOnInit(): void {
+    this.images=[
+      {image:"assets/blog-img212.jpg"},
+      {image:"assets/blog-img218.jpg"},
+      {image:"assets/blog-img220.jpg"},
+      {image:"assets/blog-img219.jpg"},
+      {image:"assets/blog-img211.jpg"},
+      {image:"assets/blog-img222.jpg"}
+    ]
   }
     
 }
